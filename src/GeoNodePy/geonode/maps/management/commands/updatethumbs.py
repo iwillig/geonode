f4ce96004d6edc9fb5d05d3038d8260c70515552
Ian Schneider <ischneider@opengeo.org>
2012-01-07 07:56:45 -0700

    management script to regenerate thumbs

---
+++ src/GeoNodePy/geonode/maps/management/commands/updatethumbs.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from geonode.maps.models import Thumbnail
+import os
+
+class Command(BaseCommand):
+    help = 'Regenerate thumbnails'
+    args = '[all]'
+
+    def handle(self, *args, **keywordargs):
+        all = 'all' in args
+        for t in Thumbnail.objects.all():
+            if all or not os.path.exists(t.get_thumbnail_path()):
+                t.generate_thumbnail()
