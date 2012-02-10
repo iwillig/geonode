fb51c3cb2df7c1b26b4d475564e4d0fa800cee56
Ian Schneider <planablediglet@gmail.com>
2012-02-10 16:56:16 -0700

    add names for search urls

--- src/GeoNodePy/geonode/simplesearch/urls.py
+++ src/GeoNodePy/geonode/simplesearch/urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('geonode.simplesearch.views',
     url(r'^search/?$', 'new_search_page', name='new_search'),
     url(r'^search/api?$', 'new_search_api', name='new_search_api'),
     #override search urls with specific views
-    url(r'^data/search$','new_search_page',kwargs={'bytype':'layer'}),
-    url(r'^maps/search$','new_search_page',kwargs={'bytype':'map'}),
+    url(r'^data/search$','new_search_page',kwargs={'bytype':'layer'}, name='search_layers'),
+    url(r'^maps/search$','new_search_page',kwargs={'bytype':'map'}, name='search_maps'),
 )
