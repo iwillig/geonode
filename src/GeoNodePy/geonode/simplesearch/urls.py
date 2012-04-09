040af5e6878bde2d721ebeb2b267fbb3da8f2592
Ian Schneider <planablediglet@gmail.com>
2012-04-08 21:54:06 -0600

    change search urls for mapstory

--- src/GeoNodePy/geonode/simplesearch/urls.py
+++ src/GeoNodePy/geonode/simplesearch/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('geonode.simplesearch.views',
     url(r'^search/api?$', 'new_search_api', name='new_search_api'),
     url(r'^search/sapi?$', 'new_search_api_reduced', name='new_search_api_reduced'),
     #override search urls with specific views
-    url(r'^data/search$','new_search_page',kwargs={'bytype':'layer'}, name='search_layers'),
-    url(r'^maps/search$','new_search_page',kwargs={'bytype':'map'}, name='search_maps'),
+    url(r'^storylayers/search$','new_search_page',kwargs={'bytype':'layer'}, name='search_layers'),
+    url(r'^mapstories/search$','new_search_page',kwargs={'bytype':'map'}, name='search_maps'),
 )
