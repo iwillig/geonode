a4e811ce0be5826f04668891763e7f4a8477202f
Ian Schneider <planablediglet@gmail.com>
2012-05-15 16:09:38 -0600

    add jdate to str function

--- src/GeoNodePy/geonode/simplesearch/util.py
+++ src/GeoNodePy/geonode/simplesearch/util.py
@@ -1,5 +1,7 @@
 import math
 
+'''taken from http://oneau.wordpress.com/2010/06/20/julian-date-calculator/'''
+
 MJD0 = 2400000.5 # 1858 November 17, 00:00:00 hours
 
 def base60_to_decimal(xyz,delimiter=None):
@@ -23,6 +25,43 @@ def base60_to_decimal(xyz,delimiter=None):
 
     decimal_value = -decimal_value if sign == -1 else decimal_value
     return decimal_value
+
+def decimal_to_base60(deci,precision=1e-8):
+    """Converts decimal number into sexagesimal number parts.
+
+    ``deci`` is the decimal number to be converted. ``precision`` is how
+    close the multiple of 60 and 3600, for example minutes and seconds,
+    are to 60.0 before they are rounded to the higher quantity, for
+
+    example hours and minutes.
+    """
+    sign = "+" # simple putting sign back at end gives errors for small
+                # deg. This is because -00 is 00 and hence ``format``,
+                # that constructs the delimited string will not add '-'
+
+                # sign. So, carry it as a character.
+    if deci < 0:
+        deci = abs(deci)
+        sign = "-"
+
+    frac1, num = math.modf(deci)
+    num = int(num) # hours/degrees is integer valued but type is float
+    frac2, frac1 = math.modf(frac1*60.0)
+    frac1 = int(frac1) # minutes is integer valued but type is float
+
+    frac2 *= 60.0 # number of seconds between 0 and 60
+
+    # Keep seconds and minutes in [0 - 60.0000)
+    if abs(frac2 - 60.0) < precision:
+        frac2 = 0.0
+
+        frac1 += 1
+    if abs(frac1 - 60.0) < precision:
+        frac1 = 0.0
+
+        num += 1
+
+    return (sign,num,frac1,frac2)
 
 def julian_date(year,month,day,hour=0,minute=0,second=0):
     """Given year, month, day, hour, minute and second return JD.
@@ -58,6 +97,46 @@ def julian_date(year,month,day,hour=0,minute=0,second=0):
 
     return MJD0 + mjdmidnight + fracofday
 
+def caldate(mjd):
+    """Given mjd return calendar date.
+
+    Retrns a tuple (year,month,day,hour,minute,second). The last is a
+    floating point number and others are integers. The precision in
+    seconds is about 1e-4.
+
+    To convert jd to mjd use jd - 2400000.5. In this module 2400000.5 is
+    stored in MJD0.
+
+    """
+    MJD0 = 2400000.5 # 1858 November 17, 00:00:00 hours
+
+    modf = math.modf
+    a = long(mjd+MJD0+0.5)
+    # Julian calendar on or before 1582 October 4 and Gregorian calendar
+
+    # afterwards.
+    if a < 2299161: 
+        b = 0
+        c = a + 1524
+
+    else: 
+        b = long((a-1867216.25)/36524.25)
+        c = a+ b - long(modf(b/4)[1]) + 1525
+
+    d = long((c-122.1)/365.25)
+    e = 365*d + long(modf(d/4)[1])
+    f = long((c-e)/30.6001)
+
+    day = c - e - int(30.6001*f)
+    month = f - 1 - 12*int(modf(f/14)[1])
+    year = d - 4715 - int(modf((7+month)/10)[1])
+    fracofday = mjd - math.floor(mjd)
+    hours = fracofday * 24.0
+
+    sign,hour,minute,second = decimal_to_base60(hours)
+
+    return (year,month,day,int(sign+str(hour)),minute,second)
+
 def iso_str_to_jdate(iso_str):
     ymd = iso_str.split('T')[0]
     neg = 1
@@ -69,9 +148,16 @@ def iso_str_to_jdate(iso_str):
     args[0] *= neg
     return int(julian_date(*args))
 
+def jdate_to_approx_iso_str(jdate):
+    if jdate is None: return None
+    y,m,d,h,m,s = caldate(jdate - MJD0)
+    return '%.4d-%.2d-%.2d' % (y,m,d)
+
 def test():
     print int(julian_date(-1000000000,1,1))
-    print iso_str_to_jdate('-5000-01-01T12:00:00Z')
+    jdate = iso_str_to_jdate('-5000-01-01T12:00:00Z')
+    print caldate(jdate - MJD0)
+    
 
 if __name__ == '__main__':
     test()
