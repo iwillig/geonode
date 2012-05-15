1baae2d8b358571b9c4f82f9fd0dede254055abd
Ian Schneider <planablediglet@gmail.com>
2012-05-15 16:10:06 -0600

    enhancements to spatial-temporal indices
    - signals for delete, create and update
    
    - handle missing time data properly

--- src/GeoNodePy/geonode/simplesearch/models.py
+++ src/GeoNodePy/geonode/simplesearch/models.py
@@ -1,9 +1,13 @@
 from django.contrib.gis.db import models
 from django.contrib.gis.gdal import Envelope
+from django.db.models import signals
+
 from geonode.maps.models import Layer
 from geonode.maps.models import Map
-from logging import getLogger
+from geonode.maps.models import map_changed_signal
 from geonode.simplesearch import util
+
+from logging import getLogger
 
 _logger = getLogger(__name__)
 
@@ -15,6 +19,14 @@ class SpatialTemporalIndex(models.Model):
     
     class Meta:
         abstract = True
+        
+    def __unicode__(self):
+        return '<SpatialTemporalIndex> for %s, %s, %s - %s' % (
+            self.indexed,
+            self.extent.extent,
+            util.jdate_to_approx_iso_str(self.time_start),
+            util.jdate_to_approx_iso_str(self.time_end)
+        )
     
 class LayerIndex(SpatialTemporalIndex):
     indexed = models.OneToOneField(Layer,related_name='spatial_temporal_index')
@@ -29,8 +41,8 @@ def filter_by_period(index, start, end):
     if end:
         q = q.filter(time_end__lte = util.iso_str_to_jdate(end))
     return q
-    
-def index_object(obj):
+
+def index_object(obj, update=False):
     if type(obj) == Layer:
         index = LayerIndex
         func = index_layer
@@ -40,27 +52,38 @@ def index_object(obj):
     else:
         raise Exception('cannot index %s' % obj)
     
+    created = False
     try:
         index_obj = index.objects.get(indexed=obj)
     except index.DoesNotExist:
+        _logger.debug('created index for %s',obj)
         index_obj = index(indexed=obj)
+        created = True
     
-    func(index_obj, obj)
+    if not update or created:
+        _logger.debug('indexing %s',obj)
+        func(index_obj, obj)
+    else:
+        _logger.debug('skipping %s',obj)
         
 def index_layer(index, obj):
+    start = end = None
     try:
         start, end = obj.get_time_extent()
     except:
-        _logger.warn('could not get WMS info for %s', obj.typename)
-        return
+        _logger.warn('could not get time info for %s', obj.typename)
     
     if start:
         index.time_start = util.iso_str_to_jdate(start)
     if end:
         index.time_end = util.iso_str_to_jdate(end)
         
-    wms_metadata = obj.metadata()
-
+    try:
+        wms_metadata = obj.metadata()
+    except:
+        _logger.warn('could not get WMS info for %s', obj.typename)
+        return
+    
     min_x, min_y, max_x, max_y = wms_metadata.boundingBoxWGS84
     index.extent = Envelope(min_x,min_y,max_x,max_y).wkt;
     index.save()
@@ -70,11 +93,12 @@ def index_map(index, obj):
     time_end = None
     extent = Envelope(0,0,0,0)
     for l in obj.local_layers:
+        start = end = None
         try:
             start, end = l.get_time_extent()
         except:
-            _logger.warn('could not get WMS info for %s', l.typename )
-            continue
+            _logger.warn('could not get time info for %s', l.typename)
+
         if start:
             start = util.iso_str_to_jdate(start)
             if time_start is None:
@@ -88,7 +112,10 @@ def index_map(index, obj):
             else:
                 time_end = max(time_end, end)
             
-        wms_metadata = l.metadata()
+        try:
+            wms_metadata = l.metadata()
+        except:
+            _logger.warn('could not get WMS info for %s', l.typename )
                 
         extent.expand_to_include(wms_metadata.boundingBoxWGS84)
         
@@ -99,3 +126,27 @@ def index_map(index, obj):
     index.extent = extent.wkt
     index.save()
         
+def object_created(instance, sender, **kw):
+    if kw['created']:
+        index_object(instance)
+        
+def map_updated(sender, **kw):
+    if kw['what_changed'] == 'layers':
+        index_object(sender)
+        
+def object_deleted(instance, sender, **kw):
+    if type(instance) == Layer:
+        index = LayerIndex
+    elif type(instance) == Map:
+        index = MapIndex
+    try:
+        index.objects.get(indexed=instance).delete()
+    except index.DoesNotExist:
+        pass
+        
+signals.post_save.connect(object_created, sender=Layer)
+
+signals.pre_delete.connect(object_deleted, sender=Map)
+signals.pre_delete.connect(object_deleted, sender=Layer)
+
+map_changed_signal.connect(map_updated)
