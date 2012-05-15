8825512e30aaac940e69e5e8f461da0ec88dbb1f
Ian Schneider <planablediglet@gmail.com>
2012-05-15 13:29:13 -0600

    add alias URL to override existing

--- src/GeoNodePy/geonode/simplesearch/urls.py
+++ src/GeoNodePy/geonode/simplesearch/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('geonode.simplesearch.views',
     url(r'^search/api$', 'new_search_api', name='new_search_api'),
     url(r'^search/api/authors$', 'author_list', name='search_api_author_list'),
     url(r'^search/sapi?$', 'new_search_api_reduced', name='new_search_api_reduced'),
+    # alias this
+    url(r'^maps/search/?$', 'new_search_page', name='maps_search'),
     #override search urls with specific views
     url(r'^storylayers/search$','new_search_page',kwargs={'bytype':'layer'}, name='search_layers'),
     url(r'^mapstories/search$','new_search_page',kwargs={'bytype':'map'}, name='search_maps'),
