6b24b5c1cfe8dbd11fb54a56634d846605731ae4
Ian Schneider <planablediglet@gmail.com>
2012-05-29 11:16:48 -0600

    include layer name in query search

--- src/GeoNodePy/geonode/simplesearch/search.py
+++ src/GeoNodePy/geonode/simplesearch/search.py
@@ -290,7 +290,7 @@ def _get_layer_results(results, query, kw):
         name_filter = reduce(operator.or_,[ Q(name__regex=f) for f in _exclude_patterns])
         q = Layer.objects.exclude(name_filter).filter(publish__status='Public')
         if query:
-            q = q.filter(_build_kw_query(query,True))
+            q = q.filter(_build_kw_query(query,True)) | q.filter(name__icontains = query)
         # we can optimize kw search here
         # maps will still be slow, but this way all the layers are filtered
         # bybw before the cruddy in-memory filter
