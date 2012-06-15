666d50f49b6f5347024202b814dec039312c769a
Jeffrey Johnson <jjohnson@opengeo.org>
2012-06-15 08:44:53 -0700

    Merging in social-synth

--- src/GeoNodePy/geonode/core/admin.py
+++
@@ -1,7 +0,0 @@
-from geonode.core.models import ObjectRole, UserObjectRoleMapping, GroupObjectRoleMapping, GenericObjectRoleMapping
-from django.contrib import admin
-
-admin.site.register(ObjectRole)
-admin.site.register(UserObjectRoleMapping)
-admin.site.register(GroupObjectRoleMapping)
-admin.site.register(GenericObjectRoleMapping)
