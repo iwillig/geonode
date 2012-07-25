de249600b3c816724cad8ff98c252cd94375814a
Bart van den Eijnden <bartvde@opengeo.org>
2012-07-25 14:14:39 +0200

    https://github.com/opengeo/mapstory/issues/377

--- src/geonode-client/app/static/script/app/GeoExplorer/PlaybackToolbar.js
+++ src/geonode-client/app/static/script/app/GeoExplorer/PlaybackToolbar.js
@@ -61,6 +61,7 @@ GeoExplorer.PlaybackToolbar = Ext.extend(gxp.PlaybackToolbar,{
                 }]
             },
             'togglesize' : {
+                disabled: false,
                 iconCls:'gxp-icon-fullScreen',
                 toggleHandler: this.toggleMapSize,
                 hidden: this.layerManager == null,
@@ -69,6 +70,7 @@ GeoExplorer.PlaybackToolbar = Ext.extend(gxp.PlaybackToolbar,{
                 scope: this
             },
             'legend' : {
+                disabled: false,
                 iconCls:'gxp-icon-legend',
                 hidden: this.layerManager == null,
                 toggleHandler: this.toggleLegend,
