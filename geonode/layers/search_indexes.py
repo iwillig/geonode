e45ae760f70b78669e0fef2b4e30b62b344c2143
Jeffrey Johnson <jjohnson@opengeo.org>
2012-08-20 15:24:23 -0700

    Comment out use of resource.latlon_bbox

--- geonode/layers/search_indexes.py
+++ geonode/layers/search_indexes.py
@@ -21,10 +21,10 @@ class LayerIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
     modified = indexes.DateTimeField(model_attr="date")
     category = indexes.CharField(model_attr="topic_category", faceted=True)
     detail_url = indexes.CharField(model_attr="get_absolute_url")
-    #bbox_left = indexes.FloatField(model_attr='bbox_left')
-    #bbox_right = indexes.FloatField(model_attr='bbox_right')
-    #bbox_top = indexes.FloatField(model_attr='bbox_top')
-    #bbox_bottom = indexes.FloatField(model_attr='bbox_bottom')
+    bbox_x0 = indexes.FloatField(model_attr='bbox_x0')
+    bbox_x1 = indexes.FloatField(model_attr='bbox_x1')
+    bbox_y1 = indexes.FloatField(model_attr='bbox_y1')
+    bbox_y0 = indexes.FloatField(model_attr='bbox_y0')
 
     json = indexes.CharField(indexed=False)
 
@@ -45,7 +45,7 @@ class LayerIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
         return prepped
 		
     def prepare_json(self, obj):
-        bbox = obj.resource.latlon_bbox
+        #bbox = obj.resource.latlon_bbox
         #poc_profile = Contact.objects.get(user=obj.poc.user)
 		
         data = {
