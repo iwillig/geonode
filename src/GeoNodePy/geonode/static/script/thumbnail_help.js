cded1e138b81c1f75e51f018ca55682fde744367
Ian Schneider <planablediglet@gmail.com>
2012-09-04 11:41:44 -0600

    extract thumbnail script, enhance spec preparation
    - build thumb spec using detached element
    - strip non visible elements from spec

--- src/GeoNodePy/geonode/static/script/thumbnail_help.js
+++ src/GeoNodePy/geonode/static/script/thumbnail_help.js
@@ -1,4 +1,4 @@
-var set_thumbnail = Ext.get("set_thumbnail");
+var set_thumbnail;
 function promptThumbnail() {
     var img = '<img src="' + randURL() + '">';
     Ext.MessageBox.show({
@@ -22,16 +22,18 @@ function randURL() {
 }
 
 function updateThumbnail(interactive) {
-    var map = new Ext.Element(Ext.query(".olMapViewport")[0]);
+    var map = Ext.select('.olMapViewport').item(0), clone = new Ext.Element(map.dom.cloneNode(true));
     // walk through and hide controls
-    map.select('*').each(function(e,i) {
-        e.getAttribute('class').indexOf('olControl') >= 0 && e.hide();
+    clone.select('*').each(function(e,i) {
+        if (!e.isVisible() || e.getAttribute('class').indexOf('olControl') >= 0) {
+            e.remove();
+        }
     });
     var html = Ext.DomHelper.markup({
         style: {
             width: map.getWidth(), height:map.getHeight()
         },
-        html: map.dom.innerHTML
+        html: clone.dom.innerHTML
     });
     if (interactive) {
         Ext.MessageBox.wait("Generating Thumbnail","Please wait while the thumbnail is generated.");
@@ -53,31 +55,30 @@ function updateThumbnail(interactive) {
           }
       }
     });
-    // walk through and show controls
-    map.select('*').each(function(e,i) {
-        e.getAttribute('class').indexOf('olControl') >= 0 && e.show();
-    });
 }
-if (set_thumbnail) {
-    set_thumbnail.on("click",function(ev) {
-       ev.stopEvent();
-       promptThumbnail();
-    });
-    if (!hasThumb) {
-        var loadCnt = 0, toLoad = 0;
-        function checkload(layer) {
-            layer.events.unregister('loadend',null, checkload);
-            if (++loadCnt == toLoad) {
-                updateThumbnail(false);
-            }
-        }
-        app.mapPanel.map.events.register("addlayer",null,function(ev) {
-            if (ev.layer.visibility) {
-                toLoad++;
-                ev.layer.events.register("loadend",null,function(ev) {
-                    checkload(ev.object);
-                });
-            }
+Ext.onReady(function() {
+    set_thumbnail = Ext.get("set_thumbnail");
+    if (set_thumbnail) {
+        set_thumbnail.on("click",function(ev) {
+           ev.stopEvent();
+           promptThumbnail();
         });
+        if (!hasThumb) {
+            var loadCnt = 0, toLoad = 0;
+            function checkload(layer) {
+                layer.events.unregister('loadend',null, checkload);
+                if (++loadCnt == toLoad) {
+                    updateThumbnail(false);
+                }
+            }
+            app.mapPanel.map.events.register("addlayer",null,function(ev) {
+                if (ev.layer.visibility) {
+                    toLoad++;
+                    ev.layer.events.register("loadend",null,function(ev) {
+                        checkload(ev.object);
+                    });
+                }
+            });
+        }
     }
-}
+})
