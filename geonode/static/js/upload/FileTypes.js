d8e99d908bac48f0c61b57a8bb4feb291f6ea8c7
Ivan Willig <iwillig@gmail.com>
2012-09-25 16:29:15 -0400

    Define globals to make jslint happy

--- geonode/static/js/upload/FileTypes.js
+++ geonode/static/js/upload/FileTypes.js
@@ -1,4 +1,7 @@
-define(['./FileType'], function(FileType) {
+/*globals define: true */
+
+define(['./FileType'], function (FileType) {
+    'use strict';
     return {
         SHP: new FileType('ESRI Shapefile', 'shp', ['shp', 'prj', 'dbf', 'shx']),
         TIF: new FileType('GeoTiff File', 'tif', ['tif']),
