b065c03d1cf61030ac97bf2fb29a7e4d427f88d8
Ivan Willig <iwillig@gmail.com>
2013-02-25 15:45:55 -0500

    Add a test case to handle when the forward_mercator edge case

--- src/GeoNodePy/geonode/maps/tests.py
+++ src/GeoNodePy/geonode/maps/tests.py
@@ -1359,6 +1359,13 @@ class UtilsTest(TestCase):
         ne = forward_mercator((180, 90))
         sw = forward_mercator((-180, -90))
 
+        inf_test = forward_mercator(
+            (-8.988465674311579e+307, -8.988465674311579e+307)
+        )
+
+        self.assertEqual(inf_test[0], float('-inf'))
+        self.assertEqual(inf_test[1], float('-inf'))
+
         self.assertEqual(round(arctic[0]), 0, "Arctic longitude is correct")
         self.assertEqual(round(arctic[1]), 19971869, "Arctic latitude is correct")
 
